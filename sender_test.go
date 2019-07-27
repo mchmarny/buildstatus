fd5d13f0ec834a9202965d79e540ba1c04737676
Mark Chmarny <mark@chmarny.com>
2019-07-27 16:45:18 -0700

    Fixed sender text

--- sender_test.go
+++ sender_test.go
@@ -11,8 +11,8 @@ func TestSlackSend(t *testing.T) {
 		LogURL: "https://console.cloud.google.com/gcr/builds/7e1135b1-edd3-45f8-a66b-a94b8f323e16?project=546930041843",
 		Source: CloudBuildSource{
 			RepoSource: RepoSource{
-				RepoName: "github_mchmarny_knative-gitops-using-cloud-build",
-				TagName:  "release-v0.3.5",
+				RepoName: "github_mchmarny_buildstatus",
+				TagName:  "release-v0.1.1",
 			},
 		},
 	}
